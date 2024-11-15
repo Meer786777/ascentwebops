ed8824100e38d0f12b0d1a25ba673c581ff44981
Ibrahim brohi <133601225+Meer786777@users.noreply.github.com>
2024-11-15 08:46:44 -0800

    nav-link fixes

--- src/assets/navbar.tsx
+++ src/assets/navbar.tsx
@@ -5,14 +5,14 @@ function Navbar() {
     return (
         <div className="navbar-parent pl-4 pr-2 flex justify-between items-center">
             <div className='left'>
-                <Link className='nav-logo' to="/">AscentWebOps</Link>
+                <Link className='nav-logo' to="ascentwebops/">AscentWebOps</Link>
             </div>
 
             <div className='right flex justify-between items-center gap-2'>
-                <Link className='nav-link' to="/services">Services</Link>
-                <Link className='nav-link' to="/about">About</Link>
+                <Link className='nav-link' to="ascentwebops/services">Services</Link>
+                <Link className='nav-link' to="ascentwebops/about">About</Link>
                 <div className='bg-black contact-btn flex justify-center items-center'>
-                    <Link className='nav-link-con' to="/contact">Contact</Link>
+                    <Link className='nav-link-con' to="ascentwebops/contact">Contact</Link>
                 </div>
             </div>
         </div>
