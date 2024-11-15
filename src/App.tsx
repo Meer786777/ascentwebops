8323fdfcb6ab792996430bb0e9b34ded198bb91c
Ibrahim brohi <133601225+Meer786777@users.noreply.github.com>
2024-11-15 08:43:04 -0800

    push

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,10 @@ function App() {
       <Navbar />
       <div className='App'>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/services' element={<Services />} />
-          <Route path='/Contact' element={<Contact />} />
+          <Route path='ascentwebops/' element={<Home />} />
+          <Route path='ascentwebops/about' element={<About />} />
+          <Route path='ascentwebops/services' element={<Services />} />
+          <Route path='ascentwebops/Contact' element={<Contact />} />
         </Routes>
       </div>
       <div className='App'>
